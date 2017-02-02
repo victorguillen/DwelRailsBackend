c9b8c88369fc2086c83c7dc21c049a574f6fb09e
Victor Guillen <victorguillen@Victors-MacBook-Air.local>
2017-02-01 20:15:51 -0800

    Fixed Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,73 +1,14 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'comments/index'
+
+  root to: 'static_pages#root'
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [ :show, :create ]
+    resources :groups, only: [ :index, :show, :create, :update]
+    resource :session, only: [ :create, :destroy ]
+    resources :todos, only: [ :index, :create, :update ]
+    resources :comments, only: [ :index, :create]
   end
-
-  namespace :api do
-    get 'comments/show'
-  end
-
-  namespace :api do
-    get 'comments/create'
-  end
-
-  namespace :api do
-    get 'groups/index'
-  end
-
-  namespace :api do
-    get 'groups/show'
-  end
-
-  namespace :api do
-    get 'groups/create'
-  end
-
-  namespace :api do
-    get 'groups/update'
-  end
-
-  namespace :api do
-    get 'groups/destroy'
-  end
-
-  namespace :api do
-    get 'session/create'
-  end
-
-  namespace :api do
-    get 'session/destroy'
-  end
-
-  namespace :api do
-    get 'users/create'
-  end
-
-  namespace :api do
-    get 'users/show'
-  end
-
-  namespace :api do
-    get 'todos/index'
-  end
-
-  namespace :api do
-    get 'todos/create'
-  end
-
-  namespace :api do
-    get 'todos/update'
-  end
-
-  get 'todos/index'
-
-  get 'todos/create'
-
-  get 'todos/update'
-
-  get 'users/create'
-
-  get 'users/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
