8e07b9151e3f363310e30e44976f386ccc0afe34
Matt McGregor <mttmcgrgr@gmail.com>
2017-02-02 20:52:13 -0800

    hmm

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,9 +2,9 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-  
     if @user.save
       login(@user)
+      debugger
       render "api/users/show"
     else
       render json: @user.errors.full_messages, status: 422
