ae9db27158925034b28f1740e7a49dc36934b991
System Administrator <root@Victors-MacBook-Air.local>
2017-02-02 20:54:59 -0800

    changes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,9 +3,9 @@ class Api::UsersController < ApplicationController
   def create
     debugger;
     @user = User.new(user_params)
-  
     if @user.save
       login(@user)
+      debugger
       render "api/users/show"
     else
       render json: @user.errors.full_messages, status: 422
